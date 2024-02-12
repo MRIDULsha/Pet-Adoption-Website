4068317386e0cf5c2ea963b268be4a24a7af3c86
MRIDULsha <106610525+MRIDULsha@users.noreply.github.com>
2024-02-12 14:56:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,33 @@ function slideshow(selector, duration) {
 }
 
 slideshow(".mainimg", 10000); 
+
+
+// //for counters
+
+
+const myNum = document.querySelectorAll('.num')
+// console.log(myNum.innerText)
+let speed = 200;
+
+myNum.forEach( (myCount) => {
+    
+    
+    let target_count = myCount.dataset.count;
+    let init_count = +myCount.innerText;
+    // console.log(target_count)
+    
+    let new_increment_num = Math.floor(target_count / speed);
+    
+    const updateNumber = () => {
+        init_count +=  new_increment_num;
+        myCount.innerText = init_count;
+        
+        if(init_count < target_count){
+            setTimeout(() => {updateNumber()}, 5)
+        }
+    }
+    
+    updateNumber();
+    
+})
